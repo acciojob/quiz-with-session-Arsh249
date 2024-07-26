98b1ee63a9b6c05c3aa08b9ce153236a091637bf
Arsh Ilahi <156427743+Arsh249@users.noreply.github.com>
2024-07-26 11:38:07 +0530

    created a submission

--- script.js
+++ script.js
@@ -46,7 +46,7 @@ document.addEventListener("DOMContentLoaded", () => {
       optionItem.classList.add("option");
       optionItem.innerHTML = `
         <label>
-          <input type="radio" name="question${index}" value="${choice}" checked}>
+          <input type="radio" name="question${index}" value="${choice}" checked="true"}>
           ${choice}
         </label>`;
       optionsList.appendChild(optionItem);
